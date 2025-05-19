292d980fd159970e3027d128b4b884bcbd03dcc6
EnochAy <enoch.ayomide.fadeyi@gmail.com>
2025-05-20 00:30:51 +0100

    Final Commits

--- solution1.sql
+++ solution1.sql
@@ -1,29 +1,3 @@
-/*Assessment_Q1.sql
-High-Value Customers with Multiple Products
-
-    This SQL query retrieves a list of customers who have both funded savings accounts and investment plans.
-    It calculates the total deposits made by each customer and orders the results by total deposits in descending order.
-
-*/
--- Find customers with both funded savings and investment plans
-SELECT
-    u.id AS owner_id,
-    CONCAT(u.first_name, ' ', u.last_name) AS name,
-    COUNT(DISTINCT s.id) AS savings_count,
-    COUNT(DISTINCT p.id) AS investment_count,
-    ROUND(SUM(s.confirmed_amount) / 100.0, 2) AS total_deposits
-FROM
-    users_customuser u
-LEFT JOIN savings_savingsaccount s ON s.owner_id = u.id AND s.confirmed_amount > 0
-LEFT JOIN plans_plan p ON p.owner_id = u.id AND p.plan_type_id = 1 AND p.amount > 0
-WHERE
-    EXISTS (
-        SELECT 1 FROM savings_savingsaccount s1 
-        WHERE s1.owner_id = u.id AND s1.confirmed_amount > 0
-    )
-    AND EXISTS (
-        SELECT 1 FROM plans_plan p1 
-        WHERE p1.owner_id = u.id AND p1.plan_type_id = 1 AND p1.amount > 0
-    )
-GROUP BY u.id, u.first_name, u.last_name
-ORDER BY total_deposits DESC;
+version https://git-lfs.github.com/spec/v1
+oid sha256:c26ff0c65fa58f7061f646b04629c05869c2b1194ad126dcf55151a259cc0707
+size 1160
--- solution3.sql
+++ solution3.sql
@@ -1,34 +1,3 @@
-/* Assessment_Q3.sql
-Account Inactivity Alert
-    This SQL query retrieves a list of customers who have both funded savings accounts and investment plans.
-    It calculates the total deposits made by each customer and orders the results by total deposits in descending order.
-*/
-
--- Find accounts (savings or investment) with no transaction in last 365 days
-WITH last_txn_dates AS (
-    SELECT
-        id AS plan_id,
-        savings_id AS owner_id,
-        'Savings' AS type,
-        MAX(transaction_date) AS last_transaction_date
-    FROM savings_savingsaccount
-    GROUP BY id, savings_id
-    UNION
-    SELECT
-        id AS plan_id,
-        owner_id,
-        'Investment' AS type,
-        MAX(last_charge_date) AS last_transaction_date
-    FROM plans_plan
-    WHERE plan_type_id = 1
-    GROUP BY id, owner_id
-)
-SELECT
-    plan_id,
-    owner_id,
-    type,
-    last_transaction_date,
-    DATEDIFF(CURDATE(), last_transaction_date) AS inactivity_days
-FROM last_txn_dates
-WHERE last_transaction_date < DATE_SUB(CURDATE(), INTERVAL 365 DAY)
-ORDER BY inactivity_days DESC;
+version https://git-lfs.github.com/spec/v1
+oid sha256:6e846cfedd0d2d6e318d83927d0cc0fe8c0e558d9959628b5f587426af8c5482
+size 1101
